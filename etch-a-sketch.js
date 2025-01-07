bb4a5ec745737debd7a5d0e3757f570bf3049b6c
Thomas Byrne <byrnesy987@gmail.com>
2025-01-07 18:07:54 +0200

    added logic for new grid button and eventlistener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,3 @@
-//const body = document.querySelector("body")
 
 const container = document.querySelector(".container")
 container.style.height = "800px"
@@ -41,14 +40,8 @@ function clearGrid() {
             console.log(container.children[i].children[j])
             container.children[i].children[j].style.backgroundColor = "";
         }
-    }
-    
-       
-       
+    }       
 };
-
-
-//clearGrid()
 
 container.addEventListener("mouseover", function(event) {
     event.target.style.backgroundColor = "purple"
@@ -57,20 +50,29 @@ container.addEventListener("mouseover", function(event) {
 const clearBtn = document.getElementById("clearButton")
 clearBtn.addEventListener("click", clearGrid)
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", function() {
+const newGridBtn = document.getElementById("newGridButton");
+newGridBtn.addEventListener("click", function() {
     let newAmount = prompt("How many grids per side? (Max 100)") 
-    while (container.firstChild) {
-            container.removeChild(container.lastChild)
-    };
+    
     newGrid(newAmount)
 })
 
 function newGrid(input) {
-    for (let i = 0; i < (input*input); i++) {
-        var grid = document.createElement("div");
-        grid.id = "row" +i;
-        grid.classList = "rowgrid"
-        container.appendChild(grid)
+    container.replaceChildren()
+    //container.appendChild(document.createElement("div"))
+    for (let i = 0; i < input; i++) {
+        let row = document.createElement("div");
+        row.id = "row" +i;
+        row.classList.add("row");
+        row.style.height = (800/input)+"px";
+        for (let y = 0; y < input; y++) {
+            let column = document.createElement("div");
+            column.id = "column" +i;
+            column.classList.add("grid")
+            column.style.width = (800/input)+"px";
+            console.log(column.style.width)
+            row.appendChild(column)
+        }
+        container.appendChild(row)
     }
 }
