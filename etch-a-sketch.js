b0311d1bf2ab524426d79290858af95ac62a5a22
Thomas Byrne <byrnesy987@gmail.com>
2025-01-07 17:53:24 +0200

    added clear button and clearGrid funtion

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,16 +5,21 @@ container.style.height = "800px"
 container.style.width = "800px"
 let defaultGridSize = 16;
 let gridSidePx = (800/defaultGridSize)
+let row;
+let column;
 console.log(gridSidePx)
+
+const buttonContainer = document.querySelector(".buttonContainer");
+buttonContainer.style.height = "50px";
 
 function createGrid(gridSideCount) {
     for (let i = 0; i < gridSideCount; i++) {
-        let row = document.createElement("div");
+        row = document.createElement("div");
         row.id = "row" +i;
         row.classList.add("row");
         row.style.height = gridSidePx+"px";
         for (let y = 0; y < gridSideCount; y++) {
-            let column = document.createElement("div");
+            column = document.createElement("div");
             column.id = "column" +i;
             column.classList.add("grid")
             column.style.width = gridSidePx+"px";
@@ -28,11 +33,29 @@ function createGrid(gridSideCount) {
 
 createGrid(defaultGridSize)
 
+//Remember to make this dynamic based on grid size if changed
+function clearGrid() {
+    for (let i = 0; i < container.children.length; i++) {
+        console.log(container.children[i])
+        for (let j = 0; j < container.children.length; j++) {
+            console.log(container.children[i].children[j])
+            container.children[i].children[j].style.backgroundColor = "";
+        }
+    }
+    
+       
+       
+};
 
+
+//clearGrid()
 
 container.addEventListener("mouseover", function(event) {
     event.target.style.backgroundColor = "purple"
 })
+
+const clearBtn = document.getElementById("clearButton")
+clearBtn.addEventListener("click", clearGrid)
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", function() {
